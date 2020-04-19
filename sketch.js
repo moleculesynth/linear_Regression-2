fa191b50aaaa8d39b1c1f5763851c0b8cc9fecd5
Travis Feldman <moleculesynth@gmail.com>
2020-04-19 18:55:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,6 @@ var b = 0;
 
 function setup() {
   createCanvas(400, 400);
-  background(51);
 }
 
 function gradientDescent() {
@@ -28,22 +27,6 @@ function gradientDescent() {
     m = m + (error * x) * learning_rate;      //for every data point, change m and b to be more accurate
     b = b + (error) * learning_rate;
   
-    //var guess_parb= a*x*x+b*x+c;
-    //var error_parab=y-guess_parb;
-    // a= a+x*x*error_parab* learning_rate;
-    // b_2=b_2+error_parab * x * learning_rate;
-    // c=c+error_parab *learning_rate;
-    // console.log(a);
-    // console.log(b_2);
-    // console.log(c);
-  }
-}
-
-        /* function drawParabola(a,b,c){
-          for (var i=0;i<400;i++){
-            point(i-1,400-(a*i*i+b*i+c))
-          }
-        } */
  
   function drawLine() {
     var x1 = 0;
@@ -57,24 +40,6 @@ function gradientDescent() {
     stroke (50, 255, 30);           //I corrected and simplified some things here
     line (x1,y1,x2,y2);
 }
-// a=((a*x1*x1)-(a*x2*x2))/2;    //we'll revist this later: notice conflicts between "b" and "b_2"
-// b_2=((b_2*x1)-(b_2*x2))/2;
-// a is width
-// b and c are height
-// var a = map(a, 0, 1, 0, width);
-// var b_2 = map(b_2, 0, 1, height, 0);
-// var c = map(c, 0, 1, height, 0);
-// drawParabola(a,b_2,c);
-
-//var c = document.getElementById("myCanvas");
-//var ctx = c.getContext("2d");
-//ctx.beginPath();
-//ctx.moveTo(x1, y1);
-//ax^2+bx+c
-//ctx.quadraticCurveTo(a*x1^2+b_2*x1+c, y1, a*x2^2+b_2*x2+c, 0);
-// either y1 and 0, or 0 and y2 works. but the first works better
-//ctx.stroke();
-//}
 
 function mousePressed(){
     var x = map(mouseX, 0, width, 0, 1);
@@ -97,46 +62,3 @@ for (var i = 0; i < data.length; i++){
       drawLine();
 }
 }
-
-// The code below has a number of issues -- with ctx, plotPoint, and so on.
-//These issues can be avoided by sticking with P5, use the reference [https://p5js.org/reference/]
-//and find solutions for drawing and mapping native to that library. 
-
-
-
-/*function plotPoint(){
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  for (var i = 0; i < data.length; i++) {
-    var x = map(data[i].x, 0, 1, 0, width);
-    var y = map(data[i].y, 0, 1, height, 0);
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.arc(x, y, 2, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-  }
-}
-function redraw(){
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  ctx.rect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
-  ctx.fill();
-}
-
-
-function draw() {
-  background(51);
-  redraw();
-  for (var i = 0; i < data.length; i++) {
-    var x = map(data[i].x, 0, 1, 0, width);
-    var y = map(data[i].y, 0, 1, height, 0);
-    fill(255);
-    stroke(255);
-    ellipse(x, y, 8, 8);
-  }
-  plotPoint();
-  if (data.length > 1) {
-*/
- 
